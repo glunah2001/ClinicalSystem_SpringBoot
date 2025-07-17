25ab66ee6f6a284b7558b75306c17e9992409cad
glunah2001 <glunah2001@gmail.com>
2025-07-17 14:12:37 -0600

    Clinical system first commit
    Scaffolding of the structure that the project will have (models, controllers, services, mappers, dtos, etc.). I have defined the environment variables and dependencies needed to develop this project.

---
+++ src/main/java/com/glunah2001/ClinicSystemAPI/ClinicSystemApplication.java
@@ -0,0 +1,14 @@
+package com.glunah2001.ClinicSystemAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSystemApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/glunah2001/ClinicSystemAPI/ClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glunah2001.ClinicSystemAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
